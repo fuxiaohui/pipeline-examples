7b276c340a6c1eab6fd3f7af903b179c7e847286
Kris <370471476@qq.com>
2019-04-24 14:38:16 +0800

    添加对Archive的使用

--- pipeline-examples/archive-build-output-artifacts/ArchiveBuildOutputArtifacts.groovy
+++ pipeline-examples/archive-build-output-artifacts/ArchiveBuildOutputArtifacts.groovy
@@ -1,18 +1,26 @@
 // This shows a simple example of how to archive the build output artifacts.
+// 归档文件的例子
 node {
-    stage "Create build output"
-    
-    // Make the output directory.
-    sh "mkdir -p output"
+    // 编译阶段
+    stage("Create build output") {
+        // Make the output directory.
+        // 创建输出的文件夹
+        sh "mkdir -p output"
 
-    // Write an useful file, which is needed to be archived.
-    writeFile file: "output/usefulfile.txt", text: "This file is useful, need to archive it."
+        // Write an useful file, which is needed to be archived.
+        // 编写需要归档文件
+        writeFile file: "output/usefulfile.txt", text: "This file is useful, need to archive it."
 
-    // Write an useless file, which is not needed to be archived.
-    writeFile file: "output/uselessfile.md", text: "This file is useless, no need to archive it."
+        // Write an useless file, which is not needed to be archived.
+        // 编写不需要归档的文件
+        writeFile file: "output/uselessfile.md", text: "This file is useless, no need to archive it."
+    }
 
-    stage "Archive build output"
-    
-    // Archive the build output artifacts.
-    archiveArtifacts artifacts: 'output/*.txt', excludes: 'output/*.md'
+    // 归档阶段
+    stage("Archive build output") {
+        // Archive the build output artifacts.
+        // 归档输出文件
+        archiveArtifacts artifacts: 'output/*.txt', excludes: 'output/*.md'
+    }
+
 }
--- pipeline-examples/archive-build-output-artifacts/README.md
+++ pipeline-examples/archive-build-output-artifacts/README.md
@@ -1,4 +1,5 @@
-# Synopsis
+# Synopsis 概要
 
 This is a simple demonstration of how to archive the build output artifacts in workspace for later use.
 
+这是一个简单的师范: 如何归档文件
