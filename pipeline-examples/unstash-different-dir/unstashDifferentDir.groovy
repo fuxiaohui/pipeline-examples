7eeeeb4d72cb2265e19ba90a2c6dead1a867f877
Kris <370471476@qq.com>
2019-04-24 18:12:46 +0800

    添加对不同jenkins节点的使用

--- pipeline-examples/unstash-different-dir/unstashDifferentDir.groovy
+++ pipeline-examples/unstash-different-dir/unstashDifferentDir.groovy
@@ -1,35 +1,40 @@
-// First we'll generate a text file in a subdirectory on one node and stash it.
-stage("first step on first node")
+// 演示在不同的jenkins节点上执行
 
-// Run on a node with the "first-node" label.
+// 在jenkins的master节点上执行
 node('master') {
-    // Make the output directory.
-    sh "mkdir -p output"
+    stage("first step on first node") {
+        // Make the output directory.
+        // 创建output文件夹
+        sh "mkdir -p output"
 
-    // Write a text file there.
-    writeFile file: "output/somefile", text: "Hey look, some text."
+        // Write a text file there.
+        // 创建文件, 并向文件中添加内容
+        writeFile file: "output/somefile", text: "Hey look, some text."
 
-    // Stash that directory and file.
-    // Note that the includes could be "output/", "output/*" as below, or even
-    // "output/**/*" - it all works out basically the same.
-    stash name: "first-stash", includes: "output/*"
+        // Stash that directory and file.
+        // Note that the includes could be "output/", "output/*" as below, or even
+        // "output/**/*" - it all works out basically the same.
+        stash name: "first-stash", includes: "output/*"
+    }
 }
 
 // Next, we'll make a new directory on a second node, and unstash the original
 // into that new directory, rather than into the root of the build.
-stage "second step on second node"
 
-// Run on a node with the "second-node" label.
-node('second-node') {
-    // Run the unstash from within that directory!
-    dir("first-stash") {
-        unstash "first-stash"
+
+// 在jenkins的second-node节点上执行
+node('master') {
+    stage("second step on second node") {
+        // Run the unstash from within that directory!
+        dir("first-stash") {
+            unstash "first-stash"
+        }
+
+        // Look, no output directory under the root!
+        // pwd() outputs the current directory Pipeline is running in.
+        sh "ls -la ${pwd()}"
+
+        // And look, output directory is there under first-stash!
+        sh "ls -la ${pwd()}/first-stash"
     }
-
-    // Look, no output directory under the root!
-    // pwd() outputs the current directory Pipeline is running in.
-    sh "ls -la ${pwd()}"
-
-    // And look, output directory is there under first-stash!
-    sh "ls -la ${pwd()}/first-stash"
 }
