2d66bfa0a367122f295256b5ad12e790b8d211f3
Kris <370471476@qq.com>
2019-04-24 14:18:30 +0800

    添加对AnsiColor的使用

--- pipeline-examples/ansi-color-build-wrapper/AnsiColorBuildWrapper.groovy
+++ pipeline-examples/ansi-color-build-wrapper/AnsiColorBuildWrapper.groovy
@@ -1,8 +1,28 @@
 // This shows a simple build wrapper example, using the AnsiColor plugin.
+// AnsiColor插件的使用
 node {
     // This displays colors using the 'xterm' ansi color map.
+    // 使用xterm的配色方式
     ansiColor('xterm') {
         // Just some echoes to show the ANSI color.
-        stage "\u001B[31mI'm Red\u001B[0m Now not"
+        // 展示所有的ANSI颜色
+        // 可参照: http://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html#256-colors
+        stage("8 Colors") {
+            for (int i = 31; i <= 37; i++) {
+                echo("\u001B[${i}mI'm Good\u001B[0m")
+            }
+        }
+
+        stage("16 Colors") {
+            for (int i = 31; i <= 37; i++) {
+                echo("\u001B[${i};1mI'm Good\u001B[0m")
+            }
+        }
+
+        stage("256 Colors") {
+            for (int i = 0; i <= 255; i++) {
+                echo("\u001b[38;5;${i}mI'm Good\u001B[0m")
+            }
+        }
     }
 }
--- pipeline-examples/ansi-color-build-wrapper/README.md
+++ pipeline-examples/ansi-color-build-wrapper/README.md
@@ -1,4 +1,6 @@
-# Synopsis
+# Synopsis 概要
 
 This shows usage of a simple build wrapper, specifically the
 AnsiColor plugin, which adds ANSI coloring to the console output.
+
+这个脚本展示如何使用构建皮肤(比如AnsiColor plugin), 在控制台日志上显示颜色
