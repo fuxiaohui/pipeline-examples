185a4a54d6857d5754a6572e227392bb3aa93d2f
Kris <370471476@qq.com>
2019-04-25 17:56:12 +0800

    添加对timestamp插件的使用

--- pipeline-examples/timestamper-wrapper/README.md
+++ pipeline-examples/timestamper-wrapper/README.md
@@ -1,5 +1,6 @@
-# Synopsis
+# Synopsis 概要
 
 This shows usage of a simple build wrapper, specifically the
 Timestamper plugin, which adds timestamps to the console output.
 
+这个项目展示了如何使用Timestamper插件, 对打印的日志添加时间
--- pipeline-examples/timestamper-wrapper/timestamperWrapper.groovy
+++ pipeline-examples/timestamper-wrapper/timestamperWrapper.groovy
@@ -1,17 +1,25 @@
 // This shows a simple build wrapper example, using the Timestamper plugin.
+// 展示如何使用Timestamper插件
 node {
     // Adds timestamps to the output logged by steps inside the wrapper.
+    // 添加timestamps{}, 指定对哪些日志添加日期
     timestamps {
         // Just some echoes to show the timestamps.
-        stage "First echo"
-        echo "Hey, look, I'm echoing with a timestamp!"
+        // 输出日志, 展示时间
+        stage("First echo") {
+            echo "Hey, look, I'm echoing with a timestamp!"
+        }
 
         // A sleep to make sure we actually get a real difference!
-        stage "Sleeping"
-        sleep 30
+        // 休眠30秒, 感受一下不同的地方(连休眠都会有日志输入, 并且也有时间)
+        stage("Sleeping") {
+            sleep 30
+        }
 
         // And a final echo to show the time when we wrap up.
-        stage "Second echo"
-        echo "Wonder what time it is now?"
+        // 最后输出一次
+        stage("Second echo") {
+            echo "Wonder what time it is now?"
+        }
     }
 }
